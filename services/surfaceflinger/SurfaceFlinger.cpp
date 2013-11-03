91f6795e02f4629094a4ba446973dcc8ee91524c
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-03 03:16:25 +0000

    ui/sf: Add support for exynos HDMI
    Change-Id: I27b34e95c04750e3dd321a2183b6cc1d89f29b5d

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -82,6 +82,10 @@
 #include <cutils/compiler.h>
 #include "DisplayUtils.h"
 
+#ifdef SAMSUNG_HDMI_SUPPORT
+#include "SecTVOutService.h"
+#endif
+
 #define DISPLAY_COUNT       1
 
 /*
@@ -186,6 +190,16 @@ SurfaceFlinger::SurfaceFlinger()
     }
     ALOGI_IF(mDebugRegion, "showupdates enabled");
     ALOGI_IF(mDebugDDMS, "DDMS debugging enabled");
+
+#ifdef SAMSUNG_HDMI_SUPPORT
+    ALOGD(">>> Run service");
+    android::SecTVOutService::instantiate();
+#if defined(SAMSUNG_EXYNOS5250)
+    mHdmiClient = SecHdmiClient::getInstance();
+    mHdmiClient->setHdmiEnable(1);
+#endif
+#endif
+
 }
 
 void SurfaceFlinger::onFirstRef()
