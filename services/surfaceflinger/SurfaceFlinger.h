911032f12d633f5553ecab4e826bb923642a9054
morckx <morckx@gmail.com>
2015-01-22 13:14:10 +0100

    Revert "sf: Enable tilerender feature only if needed"
    This reverts commit abcfc405b3983500402428a00685500bd5bb3b4a.
    
    Change-Id: Ib12dcf787c1fd14d3c7de3881a26fcb9e67dac86

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -516,8 +516,6 @@ private:
     bool mGpuTileRenderEnable;
     bool mCanUseGpuTileRender;
     Rect mUnionDirtyRect;
-    Rect mUnionDirtyRectPrev;
-    int mDRCount;
 
 #ifdef QCOM_BSP
     // Set up the DirtyRect/flags for GPU Comp optimization if required.
