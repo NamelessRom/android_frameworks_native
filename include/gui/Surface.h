116d203828f7c4e438ddc60a78424347aaf83768
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2015-02-20 17:09:49 +0100

    hax: Add back the SURFACE_SKIP_FIRST_DEQUEUE legacy EGL workaround
    Set BOARD_EGL_SKIP_FIRST_DEQUEUE to use
    
    Change-Id: Id362e4a557f4ad167cc4774160767813192d0815

--- include/gui/Surface.h
+++ include/gui/Surface.h
@@ -312,9 +312,8 @@ private:
     Region mDirtyRegion;
 
 #ifdef SURFACE_SKIP_FIRST_DEQUEUE
-    bool mDequeuedOnce;
+    bool                        mDequeuedOnce;
 #endif
-
 };
 
 }; // namespace android
