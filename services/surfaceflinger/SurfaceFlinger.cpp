95f510c6e99bb177215315a180404ac892e416b8
Alexander Martinz <eviscerationls@gmail.com>
2016-08-19 10:35:51 +0200

    Revert "surfaceflinger: Add support for Exynos HWC services"
    This reverts commit 60f8bdfb455f0c1ff61c32427cc7de0389517257.
    
    Change-Id: I504894bc34e32679cae84222afd0c414ea81e418
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -83,10 +83,6 @@
 #include <cutils/compiler.h>
 #include "DisplayUtils.h"
 
-#ifdef USES_HWC_SERVICES
-#include "ExynosHWCService.h"
-#endif
-
 #define DISPLAY_COUNT       1
 
 /*
@@ -134,10 +130,6 @@ const String16 sDump("android.permission.DUMP");
 static sp<Layer> lastSurfaceViewLayer;
 
 // ---------------------------------------------------------------------------
-
-#ifdef USES_HWC_SERVICES
-static bool notifyPSRExit = true;
-#endif
 
 SurfaceFlinger::SurfaceFlinger()
     :   BnSurfaceComposer(),
@@ -321,14 +313,6 @@ void SurfaceFlinger::bootFinished()
     // formerly we would just kill the process, but we now ask it to exit so it
     // can choose where to stop the animation.
     property_set("service.bootanim.exit", "1");
-
-#ifdef USES_HWC_SERVICES
-    sp<IServiceManager> sm = defaultServiceManager();
-    sp<android::IExynosHWCService> hwc =
-        interface_cast<android::IExynosHWCService>(sm->getService(String16("Exynos.HWCService")));
-    ALOGD("boot finished. Inform HWC");
-    hwc->setBootFinished();
-#endif
 }
 
 void SurfaceFlinger::deleteTextureAsync(uint32_t texture) {
@@ -815,19 +799,6 @@ void SurfaceFlinger::signalTransaction() {
 }
 
 void SurfaceFlinger::signalLayerUpdate() {
-#ifdef USES_HWC_SERVICES
-    if (notifyPSRExit) {
-        notifyPSRExit = false;
-        sp<IServiceManager> sm = defaultServiceManager();
-        sp<IExynosHWCService> hwcService =
-            interface_cast<android::IExynosHWCService>(
-                sm->getService(String16("Exynos.HWCService")));
-        if (hwcService != NULL)
-            hwcService->notifyPSRExit();
-        else
-            ALOGE("HWCService::notifyPSRExit failed");
-    }
-#endif
     mEventQueue.invalidate();
 }
 
@@ -1012,9 +983,6 @@ void SurfaceFlinger::handleMessageRefresh() {
         doDebugFlashRegions();
         doComposition();
         postComposition();
-#ifdef USES_HWC_SERVICES
-        notifyPSRExit = true;
-#endif
     }
 
     previousExpectedPresent = mPrimaryDispSync.computeNextRefresh(0);
